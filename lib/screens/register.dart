43052b76880db00d5ac18eb55a4c5d3e4840a04c
Sharangan07 <109034753+Sharangan07@users.noreply.github.com>
2023-05-16 22:23:27 +0530

    Login registration form ui design

---
+++ lib/screens/register.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import '../reusable_widgets/reusable_widgets.dart';
+import 'package:project2/screens/login.dart';
+
+class register extends StatefulWidget {
+  const register({Key? key}) : super(key: key);
+
+  @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _userNameTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Register page",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(25, 176, 47, 1),
+              Color.fromRGBO(0, 0, 0, 10)
+            ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+          ),
+          child: SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter User Name", Icons.person_outline,
+                    false, _userNameTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField(
+                    "Enter Email Id", Icons.email, false, _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter phone number", Icons.phone, false,
+                    _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outlined, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                firebaseUIButton(context, "Register", () {}),
+                loginOption(),
+              ],
+            ),
+          ))),
+    );
+  }
+
+  Row loginOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Already have an account?",
+            style: TextStyle(color: Colors.white70, fontSize: 15)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => login_screen()));
+          },
+          child: const Text(
+            "  Login",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        )
+      ],
+    );
+  }
+}
